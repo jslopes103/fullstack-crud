13eb07b4502f7c7fc854e9eb21187b1bb131c687
jslopes103 <jessica.slopes103@gmail.com>
2021-12-09 23:06:13 -0300

    removing mongo url

--- server/index.js
+++ server/index.js
@@ -9,18 +9,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-    "mongodb+srv://root:root@cluster0.rx7qa.mongodb.net/fullstack-crud?retryWrites=true&w=majorit",
-    { useNewUrlParser: true }
-).then(() => {
+mongoose
+  .connect("<MONGO_URL>", { useNewUrlParser: true })
+  .then(() => {
     console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
-});
+  });
 
 require("./app/routes/person.route")(app);
 
 app.listen(9000, () => {
-    console.log("Server is listening on port 9000");
+  console.log("Server is listening on port 9000");
 });
